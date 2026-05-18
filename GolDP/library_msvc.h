82eb2d26492db109f4ebe15be6e40ae931fc34f8
jonschz <17198703+jonschz@users.noreply.github.com>
2026-05-18 10:27:48 +0900

    Match library for GOLDP (#289)
    ---------
    
    Co-authored-by: jonschz <jonschz@users.noreply.github.com>

--- GolDP/library_msvc.h
+++ GolDP/library_msvc.h
@@ -1,6 +1,15 @@
 #ifdef 0
 // For GOLDP symbols only
 
+// LIBRARY: GOLDP 0x1004b268
+// __global_unwind2
+
+// LIBRARY: GOLDP 0x1004b2aa
+// __local_unwind2
+
+// LIBRARY: GOLDP 0x1004b33e
+// __NLG_Notify
+
 // aka `operator delete`
 // LIBRARY: GOLDP 0x1004b356
 // ??3@YAXPAX@Z
@@ -17,6 +26,9 @@
 
 // LIBRARY: GOLDP 0x1004b493
 // ??_M@YGXPAXIHP6EX0@Z@Z
+
+// LIBRARY: GOLDP 0x1004b513
+// ?__ArrayUnwind@@YGXPAXIHP6EX0@Z@Z
 
 // LIBRARY: GOLDP 0x1004ba3b
 // _isalpha
@@ -45,8 +57,20 @@
 // LIBRARY: GOLDP 0x1004b930
 // _strncmp
 
+// LIBRARY: GOLDP 0x1004bb06
+// __toupper_lk
+
+// LIBRARY: GOLDP 0x1004bc48
+// __lseek_lk
+
 // LIBRARY: GOLDP 0x1004bd2e
 // __errno
+
+// LIBRARY: GOLDP 0x1004bd37
+// ___doserrno
+
+// LIBRARY: GOLDP 0x1004bd57
+// __sopen
 
 // LIBRARY: GOLDP 0x1004bd40
 // __open
@@ -54,11 +78,20 @@
 // LIBRARY: GOLDP 0x1004c026
 // __close
 
+// LIBRARY: GOLDP 0x1004c083
+// __close_lk
+
 // LIBRARY: GOLDP 0x1004c106
 // __read
 
+// LIBRARY: GOLDP 0x1004c16b
+// __read_lk
+
 // LIBRARY: GOLDP 0x1004c344
 // __write
+
+// LIBRARY: GOLDP 0x1004c3a9
+// __write_lk
 
 // LIBRARY: GOLDP 0x1004c534
 // __commit
@@ -72,6 +105,22 @@
 // LIBRARY: GOLDP 0x1004c73d
 // __amsg_exit
 
+// LIBRARY: GOLDP 0x1004cfdc
+// __mtinit
+
+// LIBRARY: GOLDP 0x1004d030
+// __mtterm
+
+// LIBRARY: GOLDP 0x1004d061
+// __getptd
+
+// LIBRARY: GOLDP 0x1004d04e
+// __initptd
+
+// LIBRARY: GOLDP 0x1004d0c8
+// __freeptd
+
+// TODO #297: Seems to mismatch, maybe a custom allocator is used?
 // LIBRARY: GOLDP 0x1004d21f
 // _free
 
@@ -81,8 +130,151 @@
 // LIBRARY: GOLDP 0x1004d420
 // __output
 
+// LIBRARY: GOLDP 0x1004dc2a
+// _malloc
+
 // LIBRARY: GOLDP 0x1004dc3c
 // __nh_malloc
+
+// TODO #297: Seems to mismatch, maybe a custom allocator is used?
+// LIBRARY: GOLDP 0x1004dc68
+// __heap_alloc
+
+// LIBRARY: GOLDP 0x1004e20c
+// __except_handler3
+
+// LIBRARY: GOLDP 0x1004e2e4
+// __cinit
+
+// LIBRARY: GOLDP 0x1004e311
+// __exit
+
+// LIBRARY: GOLDP 0x1004e322
+// __cexit
+
+// LIBRARY: GOLDP 0x1004f8fc
+// __ioinit
+
+// LIBRARY: GOLDP 0x1004f24c
+// __isctype
+
+// LIBRARY: GOLDP 0x1004f2c1
+// __mtinitlocks
+
+// LIBRARY: GOLDP 0x1004f2ea
+// __mtdeletelocks
+
+// LIBRARY: GOLDP 0x1004f356
+// __lock
+
+// LIBRARY: GOLDP 0x1004f3b7
+// __unlock
+
+// LIBRARY: GOLDP 0x1004f3cc
+// ___crtLCMapStringA
+
+// LIBRARY: GOLDP 0x1004f61b
+// __alloc_osfhnd
+
+// LIBRARY: GOLDP 0x1004f73e
+// __set_osfhnd
+
+// LIBRARY: GOLDP 0x1004f7ba
+// __free_osfhnd
+
+// LIBRARY: GOLDP 0x1004f839
+// __get_osfhandle
+
+// LIBRARY: GOLDP 0x1004f87b
+// __lock_fhandle
+
+// LIBRARY: GOLDP 0x1004f8da
+// __unlock_fhandle
+
+// LIBRARY: GOLDP 0x1004fab8
+// __ioterm
+
+// LIBRARY: GOLDP 0x1004fb0c
+// __chsize_lk
+
+// LIBRARY: GOLDP 0x1004fc31
+// __setenvp
+
+// LIBRARY: GOLDP 0x1004fcea
+// __setargv
+
+// LIBRARY: GOLDP 0x1004ff37
+// ___crtGetEnvironmentStringsA
+
+// LIBRARY: GOLDP 0x100501de
+// __heap_init
+
+// LIBRARY: GOLDP 0x1005023b
+// __heap_term
+
+// LIBRARY: GOLDP 0x100502e9
+// __FF_MSGBANNER
+
+// LIBRARY: GOLDP 0x10050322
+// __NMSG_WRITE
+
+// TODO #297: Seems to mismatch, maybe a custom allocator is used?
+// LIBRARY: GOLDP 0x100508f5
+// _calloc
+
+// LIBRARY: GOLDP 0x10051c5d
+// __getbuf
+
+// LIBRARY: GOLDP 0x10051ca1
+// __isatty
+
+// LIBRARY: GOLDP 0x10051e30
+// _strlen
+
+// LIBRARY: GOLDP 0x10051eab
+// _wctomb
+
+// LIBRARY: GOLDP 0x10051f04
+// __wctomb_lk
+
+// LIBRARY: GOLDP 0x10051f70
+// __aulldiv
+
+// LIBRARY: GOLDP 0x10051fe0
+// __aullrem
+
+// LIBRARY: GOLDP 0x10052055
+// __callnewh
+
+// LIBRARY: GOLDP 0x10052890
+// _strcpy
+
+// LIBRARY: GOLDP 0x100528a0
+// _strcat
+
+// LIBRARY: GOLDP 0x10052980
+// _memset
+
+// LIBRARY: GOLDP 0x100529d8
+// __fptrap
+
+// LIBRARY: GOLDP 0x10052df8
+// ___crtGetStringTypeA
+
+// LIBRARY: GOLDP 0x10052fd0
+// __chkstk
+
+// LIBRARY: GOLDP 0x10052fff
+// __setmode_lk
+
+// LIBRARY: GOLDP 0x10053438
+// ___initmbctable
+
+// LIBRARY: GOLDP 0x100537c0
+// ___crtMessageBoxA
+
+// GLOBAL: GOLDP 0x1004bcbb
+// __dosmaperr
 
 // GLOBAL: GOLDP 0x10058098
 // IID_IDirectDraw4
@@ -96,6 +288,114 @@
 // GLOBAL: GOLDP 0x10058468
 // IID_IDirect3DTexture2
 
+// GLOBAL: GOLDP 0x10059990
+// ___lookuptable
+
+// GLOBAL: GOLDP 0x1005f440
+// __NLG_Destination
+
+// GLOBAL: GOLDP 0x1005f600
+// ___tlsindex
+
+// GLOBAL: GOLDP 0x1005f97c
+// ___mb_cur_max
+
+// GLOBAL: GOLDP 0x1005f988
+// __locktable
+
+// GLOBAL: GOLDP 0x1005fc10
+// __XcptActTab
+
+// GLOBAL: GOLDP 0x10065f08
+// ___error_mode
+
+// GLOBAL: GOLDP 0x1005f608
+// ___nullstring
+
+// GLOBAL: GOLDP 0x1005f60c
+// ___wnullstring
+
+// GLOBAL: GOLDP 0x1005f770
+// __pctype
+
+// GLOBAL: GOLDP 0x1005fb58
+// ___badioinfo
+
+// GLOBAL: GOLDP 0x10065f00
+// __aenvptr
+
+// GLOBAL: GOLDP 0x10065f0c
+// ___app_type
+
+// GLOBAL: GOLDP 0x10065f10
+// __umaskval
+
+// GLOBAL: GOLDP 0x10065f14
+// __osver
+
+// GLOBAL: GOLDP 0x10065f18
+// __winver
+
+// GLOBAL: GOLDP 0x10065f1c
+// __winmajor
+
+// GLOBAL: GOLDP 0x10065f20
+// __winminor
+
+// GLOBAL: GOLDP 0x10065f30
+// __environ
+
+// GLOBAL: GOLDP 0x10065f4c
+// __C_Termination_Done
+
+// GLOBAL: GOLDP 0x10065fdc
+// ___lc_codepage
+
+// GLOBAL: GOLDP 0x10065fe8
+// __fmode
+
+// GLOBAL: GOLDP 0x100660f4
+// __adbgmsg
+
+// GLOBAL: GOLDP 0x10066100
+// __cflush
+
+// GLOBAL: GOLDP 0x10066104
+// __newmode
+
+// GLOBAL: GOLDP 0x10066108
+// ?_pnhHeap@@3P6AHI@ZA
+
+// GLOBAL: GOLDP 0x10067500
+// ___pioinfo
+
+// GLOBAL: GOLDP 0x10067604
+// ___setlc_active
+
+// GLOBAL: GOLDP 0x10067608
+// ___unguarded_readlc_active
+
+// GLOBAL: GOLDP 0x10067600
+// __nhandle
+
+// GLOBAL: GOLDP 0x1006760c
+// ___env_initialized
+
+// GLOBAL: GOLDP 0x10067610
+// ___mbctype_initialized
+
+// GLOBAL: GOLDP 0x1006761c
+// __acmdln
+
 // GLOBAL: GOLDP 0x10067620
 // __pRawDllMain
+
+/// Globals from libraries without symbols
+
+// STRING: GOLDP 0x10059b94
+static const char* ___crtLCMapStringA_str = "\0";
+
+// STRING: GOLDP 0x10059b98
+static const wchar_t* ___crtLCMapStringA_wstr = L"\0";
+
 #endif
